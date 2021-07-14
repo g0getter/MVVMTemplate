e84a58b9f998a022cb146f125bab2db0305adbf4
Aiden <nagyeong.yeo1023@gmail.com>
2021-07-14 10:59:36 +0900

    Test protocol and class

---
+++ Sources/MVVMTemplate/Test/Aiden.swift
@@ -0,0 +1,21 @@
+//
+//  Aiden.swift
+//  
+//
+//  Created by 여나경 on 2021/07/14.
+//
+
+public protocol Aiden {
+    var name: String { get }
+    func eating()
+    
+}
+
+public class AidenMini: Aiden {
+    public var name: String = "Aiden mini"
+    
+    public func eating() {
+        print("Aiden is eating something")
+    }
+    
+}
